8ca99f41f2cd68006eaaf925e337f3340f15b233
Jeff Oliver <jeffoliver3384@gmail.com>
2019-07-16 17:35:01 -0400

    created endpoint to create a car entry

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -15,4 +15,17 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const carData = req.body;
+    const [id] = await db('cars').insert(carData);
+    const newCarEntry = await db('cars').where({ id });
+
+    res.status(201).json(newCarEntry);
+  } catch (error) {
+    console.log('POST error', err);
+    res.status(500).json({ message: 'Server failed to add new car entry' });
+  }
+});
+
 module.exports = router;
